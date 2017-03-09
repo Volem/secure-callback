9fb417930c58ecc3a09995f557f6a252983043c8
Volem <volkan.metin@arsevo.com>
2017-03-09 22:34:42 +0300

    Finalize test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 var SecureCallback = require('../dist/securecallback');
 var assert = require('assert');
-var expect = require('expect');
 var scThrowsException = new SecureCallback(true);
 var scDefault = new SecureCallback();
 var scThrowsExceptionWithMsg = new SecureCallback(true, "not a function.", 'callback required.');
@@ -8,51 +7,57 @@ var scThrowsExceptionWithMsg = new SecureCallback(true, "not a function.", 'call
 describe('secureCallback', function () {
 	describe('respond', function () {
 		it('should not throw error', function () {
-			expect(() => {
+			assert.doesNotThrow(function () {
 				scDefault.respond(function () {
 					return;
-				});
-			}).toNotThrow(Error);
+				}, "arg1", "arg2");
+			}, Error)
 		});
 	});
 });
 
 describe('secureCallback', function () {
 	describe('respond', function () {
-		it('should throw error callback should be defined.', function () {
-			expect(() => {
+		it('should throw error : callback should be defined.', function () {
+			assert.throws(function () {
 				scThrowsException.respond();
-			}).toThrow(Error, 'callback should be defined.');
+			}, /callback should be defined./);
 		});
 	})
 });
 
 describe('secureCallback', function () {
 	describe('respond', function () {
-		it('should throw error callback is not a function.', function () {
+		it('should throw error :  callback is not a function.', function () {
 			var notaFunction = new Array(1);
-			expect(() => { scThrowsException.respond(notaFunction) }).toThrow(Error, 'callback is not a function.');
+			assert.throws(function () {
+				scThrowsException.respond(notaFunction);
+			}, /callback is not a function./);
 		});
 	});
+});
+
+describe('secureCallback', function () {
+	describe('respond', function () {
+		it('should throw error : callback required.', function () {
+			assert.throws(function () {
+				scThrowsExceptionWithMsg.respond();
+			}, /callback required./);
+		});
+	})
 });
 
 describe('secureCallback', function () {
 	var testObj = new SecureCallback(true);
 	describe('respond', function () {
-		it('should throw error not a function.', function () {
+		it('should throw error : not a function.', function () {
 			var notaFunction = new Array(1);
-			expect(() => { scThrowsExceptionWithMsg.respond(notaFunction) }).toThrow(Error, 'not a function.');
+			assert.throws(function () {
+				scThrowsException.respond(notaFunction);
+			}, /not a function./);
 		});
 	});
 });
 
-describe('secureCallback', function () {
-	describe('respond', function () {
-		it('should throw error callback required.', function () {
-			expect(() => {
-				scThrowsExceptionWithMsg.respond();
-			}).toThrow(Error, 'callback required.');
-		});
-	})
-});
+
 
