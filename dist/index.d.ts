50b5c7934c3bc175b7710a1ec729e2bdf9acbb74
Volem <volkan.metin@arsevo.com>
2017-03-10 01:13:24 +0300

    Added type definitions

---
+++ dist/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for [secure-callback] [1.1.7]
+// Project: [Secure Callback]
+// Definitions by: [volem] <[nodejs.withvolkan.com]>
+
+/*~ This declaration specifies that the class constructor function
+ *~ is the exported object from the file
+ */
+export = SecureCallback;
+
+declare class SecureCallback {
+    constructor(throwException: Boolean = false, notFunctionMsg: string = null, callbackRequiredMsg: string = null);
+
+    respond(callback: Function, ...args) : void;
+}
